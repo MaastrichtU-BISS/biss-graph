405f683c8dc9c21bf06ef79a42c939a9cf2d0f92
Carlos <carloseam94@gmail.com>
2024-03-19 15:49:36 +0100

    feat: load graph

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -1,9 +1,21 @@
 <template>
-    hola
+    {{ graph }}
 </template>
 <script setup lang="ts">
 
+import { ref, onMounted } from 'vue';
 
+const graphLocation: string = "/graph.json";
+const graph = ref(null);
+
+const loadGraph = async (): Promise<any> => {
+    return (await fetch(graphLocation)).json();
+}
+
+onMounted(async () => {
+    graph.value = await loadGraph();
+    console.log(graph.value);
+})
 </script>
 
 <style scoped></style>

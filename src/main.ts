37dbfec929c709ca9528bee81bd2b85c1f419a17
Carlos <carloseam94@gmail.com>
2024-03-20 12:42:56 +0100

    feat: add tailwind

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
-import './style.css'
-import 'primevue/resources/themes/aura-light-green/theme.css'
 import App from './App.vue'
 import PrimeVue from 'primevue/config';
+import './style.css'
+import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primeicons/primeicons.css'
 
 const app = createApp(App);
 app.mount('#app');

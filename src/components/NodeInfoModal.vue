40545744184cce982308b75ac26c0d7dc8377347
Carlos <carloseam94@gmail.com>
2024-10-25 14:43:27 +0200

    testing: diferent layouts

--- src/components/NodeInfoModal.vue
+++ src/components/NodeInfoModal.vue
@@ -1,17 +1,22 @@
 <template>
     <Dialog v-model:visible="isVisible" modal :style="{ width: '80vw', height: '80vh' }" dismissableMask
         :breakpoints="{ '1199px': '75vw', '575px': '90vw' }">
-        <iframe :src="`${consts.bissWebsiteURL}/team/iframe/${props.nodeInfo.id}`" frameborder="0" class="w-full h-[80vh]"></iframe>
+        <iframe :src="`${consts.bissWebsiteURL}/${nodeType}/iframe/${props.nodeInfo.id}`" frameborder="0" class="w-full h-[80vh]"></iframe>
     </Dialog>
 </template>
 <script setup lang="ts">
 import Dialog from 'primevue/dialog';
-import type { GraphNode } from "../types/graph";
+import { computed } from "vue";
 import * as consts from "../consts";
+import { type Node, NodeType } from "../types/graph";
 
 const props = defineProps<{
-    nodeInfo: GraphNode
+    nodeInfo: Node
 }>();
+
+const nodeType = computed( () => {
+    return props.nodeInfo.group == NodeType.PROJECT ? "posts" : "team";
+});
 
 const isVisible = defineModel<boolean>('isVisible', { required: true, default: false} );
 

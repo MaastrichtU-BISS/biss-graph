e58ec1c2934a7ace6679e3efeb971f9c6818f5a0
CvdLans <93982741+CvdL-UM@users.noreply.github.com>
2025-05-20 14:15:22 +0200

    feat: Log instructions for the big screen to the terminal

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,26 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),
+  {
+    name: 'post-start-log',
+    configureServer(server) {
+      server.httpServer.once('listening', () => {
+        const address = server.httpServer.address();
+        setTimeout(() => {
+          console.info()
+          console.info(' _________________________________________')
+          console.info('| INSTRUCTIONS FOR THE BIG SCREEN         |');
+          console.info(`| 1. Open http://localhost:${address.port} in Chrome |`);
+          console.info(`| 2. Go full screen                       |`);
+          console.info(`|   - Three dots top right                |`);
+          console.info(`|   - Square icon halfway the menu on the |`);
+          console.info(`|     right (Full screen)                 |`);
+          console.info(`| 3. Refresh the page (Ctrl + Shift + R)  |`);
+          console.info('|_________________________________________|')
+        }, 10);
+      });
+    }
+  }
+  ],
 })
